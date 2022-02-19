bec2b8a3078288d616990dc1bee0c789d09ae363
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2022-02-19 19:45:37 +0530

    Added contact info

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#1D3251',
+      secondary: '#F2F2F2',
+      ...colors,
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      rockwell: ['Rockwell Nova', 'sans-serif'],
+      corbel: ['Corbel', 'sans-serif'],
+    },
+    extend: {
+      gridTemplateColumns: {
+        body: '35% auto',
+      },
+      gridTemplateRows: {
+        aside: '30% auto',
+      },
+    },
   },
   plugins: [],
 };
