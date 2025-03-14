0d5e62dc1d24f37f7891a2052fe250914949f1c4
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2025-03-14 13:51:52 +0000

    Upgrading tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['./src/**/*.html'],
-  theme: {
-    colors: {
-      primary: '#1D3251',
-      secondary: '#F2F2F2',
-      txtSecondary: '#666666',
-      ...colors,
+    content: ['./src/**/*.html'],
+    theme: {
+        colors: {
+            primary: '#1D3251',
+            secondary: '#F2F2F2',
+            txtSecondary: '#666666',
+            ...colors
+        },
+        letterSpacing: {
+            widest: '0.3em'
+        },
+        fontFamily: {
+            roboto: ['Roboto', 'sans-serif'],
+            rockwell: ['Rockwell Nova', 'sans-serif'],
+            corbel: ['Corbel', 'sans-serif']
+        },
+        extend: {
+            gridTemplateColumns: {
+                body: '33% auto'
+            },
+            gridTemplateRows: {
+                aside: '20% auto'
+            }
+        }
     },
-    letterSpacing: {
-      widest: '0.3em',
-    },
-    fontFamily: {
-      roboto: ['Roboto', 'sans-serif'],
-      rockwell: ['Rockwell Nova', 'sans-serif'],
-      corbel: ['Corbel', 'sans-serif'],
-    },
-    extend: {
-      gridTemplateColumns: {
-        body: '33% auto',
-      },
-      gridTemplateRows: {
-        aside: '20% auto',
-      },
-    },
-  },
-  plugins: [],
+    plugins: []
 };
